e6df2c1e9735b45e8c32fde626ef3994d3224e07
mailkey <mailkey@yeah.net>
2022-08-16 13:48:16 +0800

    Update HiStart.h

--- HiEasyX/HiEasyX/HiStart.h
+++ HiEasyX/HiEasyX/HiStart.h
@@ -1,7 +1,8 @@
 /**
  * @file	HiStart.h
  * @brief	HiEasyX 库的开始界面渲染模块
- * @author	huidong
+ * @note	开场动画改编自慢羊羊的《艺术字系列：冰封的 EasyX》
+ * @author	慢羊羊 <yw80@qq.com>，huidong（改编）
 */
 
 #pragma once
@@ -9,7 +10,7 @@
 namespace HiEasyX
 {
 	/**
-	 * @brief 渲染开始界面
+	 * @brief 渲染开场动画
 	*/
 	void RenderStartScene();
 }
