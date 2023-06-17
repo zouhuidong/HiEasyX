b00c680983b4c0ad6ba4624daa92212aab1ddddd
Alan-CRL <92425617+Alan-CRL@users.noreply.github.com>
2023-06-17 21:05:16 +0800

    Ver 0.3.4

--- HiEasyX/HiEasyX/HiDef.h
+++ HiEasyX/HiEasyX/HiDef.h
@@ -1,17 +1,17 @@
 /**
  * @file	HiDef.h
- * @brief	HiEasyX ������ö���
+ * @brief	HiEasyX 库的配置定义
  * @author	huidong
 */
 
-// �汾����
-#define _HIEASYX_VER_STR_	L"Ver 0.3.3"
+// 版本定义
+#define _HIEASYX_VER_STR_	L"Ver 0.3.4"
 
-// ʹ��ԭ�� EasyX ���ڣ�Ĭ��Ϊ HiWindow��
+// 使用原生 EasyX 窗口（默认为 HiWindow）
 //#define _NATIVE_EASYX_
 
-// ȡ�� Release ģʽ�ĳ�����������
+// 取消 Release 模式的程序启动动画
 #define _NO_START_ANIMATION_
 
-// ȡ��ϵͳ�ؼ����ִ���ʽ
+// 取消系统控件的现代样式
 //#define _NO_MORDEN_SYSCTRL_
